07182cd7713de1a7250e65f06626f5b896b266f6
Wandia E Mugo <wwandia173@gmail.com>
2022-06-05 18:13:10 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a,b) {
+  return a+b;
+}
+function subtract(a,b) {
+  return a-b;
+}
+function divide(a,b) {
+  return a/b;
+}
+function multiply (a,b){
+  return a*b;
+}
+
+function add(a,b) {
+  return a+b;
+}
+add(588,900);
+
+function subtract(a,b) {
+  return a-b;
+}
+subtract(5,137);
+
+function increment (n) {
+  return (n += 1);
+}
+increment(15);
+
+function decrement(n) {
+  return (n-=1);
+}
+decrement(5);
+
+function makeInt(n) {
+  return parseInt(n,10)
+}
+makeInt("10");
+
+function preserveDecimal(n){
+  return n=2.222;
+}
+parseFloat('2.222');
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+preserveDecimal("sldkjflksjf");
